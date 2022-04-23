fe230f518c3d8ee27ef593d0ee4df990366425e4
Yettafssaoui23 <yettafssaoui2@gmail.com>
2022-04-23 23:35:33 +0000

    Removing graphql

--- user_management/urls.py
+++ user_management/urls.py
@@ -6,11 +6,10 @@ from django.urls import path, include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.decorators.csrf import csrf_exempt
-from graphene_django.views import GraphQLView
+
 
 urlpatterns = [
     path('', include('users.urls')),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path('filiere_etab/', include('filiere.urls')),
     path('cours/', include('cours.urls')),
     path('niveau/',include('semestre.urls.niveau.urls'))
