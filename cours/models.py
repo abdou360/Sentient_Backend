08e1a933e58e50f435ba2bdd4874b1fbda378223
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 <kaoutar.oubenaddi@gmail.com>
2022-04-28 06:02:20 +0000

    modèles

--- cours/models.py
+++ cours/models.py
@@ -12,12 +12,31 @@ def upload_location(instance, filename):
         now = time.time()
         stamp = datetime.datetime.fromtimestamp(now).strftime('%Y-%m-%d-%H-%M-%S')
         return 'img/cours/chapitre_images/%s.%s' % (str(stamp), extension)
+  
+def document_image_upload_location(instance, filename):
+        filebase, extension = filename.split('.')
+        now = time.time()
+        stamp = datetime.datetime.fromtimestamp(now).strftime('%Y-%m-%d-%H-%M-%S')
+        return 'img/cours/document_images/%s.%s' % (str(stamp), extension)  
+
+def model_location(modelName):
+        now = time.time()
+        stamp = datetime.datetime.fromtimestamp(now).strftime('%Y-%m-%d-%H-%M-%S')
+        return 'img/cours/modeles_3d/%s-%s' % (modelName, str(stamp))  
+
+def model_image_location(instance, filename):
+        filebase, extension = filename.split('.')
+        now = time.time()
+        stamp = datetime.datetime.fromtimestamp(now).strftime('%Y-%m-%d-%H-%M-%S')
+        return 'img/cours/modeles_3d_images/%s.%s' % (str(stamp), extension)
     
+def file_upload_location(instance, filename):
+        modele3d = instance.modele3D.path_modele3d
+        return '%s/%s' % (modele3d, filename)
 
 class Chapitre(models.Model):
     libelle = models.CharField(max_length=40,null = False )
     description = models.CharField(max_length=250,null = True )
-    # document = models.ForeignKey(Document , null=True,  on_delete= models.SET_NULL)
     image = models.ImageField(upload_to=upload_location, default='/img/cours/folder-files-and-folders-svgrepo-com.svg')
     element_module = models.ForeignKey(ElementModule , null=True,  on_delete= models.SET_NULL)
     professeur = models.ForeignKey(Professeur,null=False, on_delete= models.CASCADE)
@@ -30,30 +49,37 @@ class Document(models.Model):
     titre = models.CharField(max_length=40,null = False )
     type = models.CharField(max_length=20,null = False )
     path = models.CharField(max_length=100,null = False )
-    image = models.ImageField(upload_to=upload_location, default=NULL)
+    image = models.ImageField(upload_to=document_image_upload_location, default=NULL)
     chapitre = models.ForeignKey(Chapitre , null=False,  on_delete= models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Modele3D(models.Model):
-    path = models.CharField(max_length=100,null = False )
+    titre_modele3d = models.CharField(max_length=40,null = False )
+    path_modele3d = models.CharField(max_length=100,null = False )
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Image(models.Model):
-    type = models.CharField(max_length=20,null = False )
-    path = models.CharField(max_length=100,null = False )
+    name_image = models.CharField(max_length=30,null = False, default='Sans Nom' )
+    is_qrcode = models.BooleanField(default=False)
+    path_image = models.FileField(upload_to=model_image_location,null = False, default=NULL )
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Traitement(models.Model):
-    titre = models.CharField(max_length=40,null = False )
-    label = models.CharField(max_length=100,null = False )
-    type = models.CharField(max_length=25,null = False )
+    titre_traitement = models.CharField(max_length=40,null = False )
+    label_traitement = models.CharField(max_length=100,null = True, blank=True)
+    type_traitement = models.CharField(max_length=25,null = False )
     chapitre = models.ForeignKey(Chapitre , null=False,  on_delete= models.CASCADE)
-    image = models.ForeignKey(Image , null=True,  on_delete= models.SET_NULL, default=NULL)
+    image = models.ForeignKey(Image , null=True, blank=True,  on_delete= models.SET_NULL, default=NULL)
     modele3D = models.ForeignKey(Modele3D , null=False,  on_delete= models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
 
+class File(models.Model):
+    modele3D = models.ForeignKey(Modele3D , null=False,  on_delete= models.CASCADE)
+    path_file = models.FileField(upload_to=file_upload_location,null = False, max_length=255 )
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
 
--- cours/views.py
+++ cours/views.py
@@ -1,11 +1,12 @@
 from distutils.command import check
+import sys
 from tkinter import Image
 from django.contrib import messages
 from django.shortcuts import redirect, render
 from pymysql import NULL
 from cours.forms import *
 
-from cours.models import Chapitre, Document, Modele3D, Traitement
+from cours.models import Chapitre, Document, Modele3D, Traitement, File
 from filiere.models import Filiere
 from module.models import ElementModule, Enseignant_Responsable, Module
 from semestre.models import Niveau, Semestre
@@ -13,13 +14,19 @@ from users.models import Professeur
 
 from django.db.models import Q
 
+import datetime
+import time
+
+import os
+
 # Create your views here.
 
 from django.contrib.auth.decorators import login_required
 
 
 def chapitres_list(request): 
-    professeur = Professeur.objects.filter(user = request.user.id).first()
+    # professeur = Professeur.objects.filter(user = request.user.id).first()
+    professeur = Professeur.objects.filter(admin_id = 1).first()
     
     search_chapitre = request.GET.get('search')
     if search_chapitre:
@@ -32,7 +39,8 @@ def chapitres_list(request):
     }
     return render(request, 'cours/chapitres.html', context) 
 def chapitre_details(request,id): 
-    professeur = Professeur.objects.filter(user = request.user.id).first()
+    # professeur = Professeur.objects.filter(user = request.user.id).first()
+    professeur = Professeur.objects.filter(admin_id = 1).first()
     chapitre = Chapitre.objects.filter(id = id).first()
     traitements = Traitement.objects.filter(chapitre_id = id).all()
     documents=Document.objects.filter(chapitre_id = id).all()
@@ -46,7 +54,8 @@ def chapitre_details(request,id):
     return render(request, 'cours/chapitre_details.html',context) 
 
 def add_chapitre(request): 
-    professeur = Professeur.objects.filter(user = request.user.id).first()
+    # professeur = Professeur.objects.filter(user = request.user.id).first()
+    professeur = Professeur.objects.filter(admin_id = 1).first()
     if request.method == "GET":
         new_chapitre = ChapitreForm()
     elif request.method == "POST":
@@ -120,28 +129,125 @@ def delete_image(request, id):
 
 def delete_Traitement(request, id):
     traitement = Traitement.objects.get(id=id)
-    # image = Image.objects.get(id=traitement.image.id)
+    
     if traitement.delete():
         messages.success(request, ('Le modele AR a été supprimé !'))
     else:
         messages.error(request, 'Erreur : Le modele  n\'a pas été supprimé')
     return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
 
+EXTENSIONS = ['pdf', 'png']
+
 def add_traitement(request,id): 
+    
     chapitre = Chapitre.objects.filter(id = id).first()
     if request.method == "GET":
         new_traitement = TraitementForm()
-    elif request.method == "POST":
-        new_traitement = TraitementForm(request.POST, request.FILES)
-        if new_traitement.is_valid():
-            Traitement = new_traitement.save(commit=False)
-            Traitement.chapitre = chapitre
-            Traitement.save()
-            messages.success(request, ('Le modele AR a été ajouté avec succès'))
-            return redirect('chapitre_details',id)
-        else:
-            messages.error(request, 'Erreur : Le chapitre n\'a pas été ajouté')
-            return redirect('add_traitement')
-    return render(request, 'cours/add_traitement.html', context={'new_traitement': new_traitement})
+        new_image = ImageForm()
+        new_modele3d = Modele3DForm()
+        new_file = FileForm()
         
+    elif request.method == "POST":
+        print('<message>', file=sys.stderr)
+        
+        new_traitement = TraitementForm(request.POST, request.FILES)
+        
+        print('<titre_modele3d>'+request.POST.get("titre_modele3d"), file=sys.stderr)
+        print('<titre_traitement>'+request.POST.get("titre_traitement"), file=sys.stderr)
+        print('<label_traitement>'+request.POST.get("label_traitement"), file=sys.stderr)
+        print('<type_traitement>'+request.POST.get("type_traitement"), file=sys.stderr)
+        
+        invalid_extension = 0
+        
+        if new_traitement.is_valid():
+            print('<traitement valid>', file=sys.stderr)
+            traitement = new_traitement.save(commit=False)
+            traitement.chapitre = chapitre
+            print('<traitement>'+traitement.type_traitement, file=sys.stderr)
+            
+            new_modele3d = Modele3DForm(request.POST, request.FILES)
+            print('<new_modele3d["titre_modele3d"]>'+new_modele3d['titre_modele3d'].value(), file=sys.stderr)
+            if new_modele3d.is_valid():
+                new_modele = new_modele3d.save(commit=False)
+                new_modele.path_modele3d = model_location(new_modele3d['titre_modele3d'].value())
+                makedirs(new_modele.path_modele3d)
+                       
+                print('<modele3d //>'+str(new_modele.id), file=sys.stderr)                
+                
+                new_modele.save()
+                
+                files = request.FILES.getlist('path_file')
+                
+                for f in files:
+                    filebase, extension = f.name.split('.')
+                    print('<file extension>'+extension, file=sys.stderr)
+                    if EXTENSIONS.__contains__(extension):
+                        print('<extension //>'+str(extension), file=sys.stderr)                
+                        print('<invalid_extension //>'+str(invalid_extension), file=sys.stderr)                
+                        ...
+                    else:
+                        print('<extension //>'+str(extension), file=sys.stderr)                
+                        print('<invalid_extension //>'+str(invalid_extension), file=sys.stderr)    
+                        invalid_extension+=1
+                        messages.error(request, 'Erreur : L\'extension n\'est pas autorisée !')
+                
+                if invalid_extension == 0:
+                    for f in files:
+                        print('<file name>'+f.name, file=sys.stderr)
+                        # print('<file extension>'+f.name, file=sys.stderr)
+                        obj = File.objects.create(modele3D = new_modele, path_file=f)
+                    
+                    if traitement.type_traitement == "Texte":
+                        print('<Texte>', file=sys.stderr)
+                        traitement.image = None
+                    else:
+                        new_image = ImageForm(request.POST, request.FILES)
+                        if new_image.is_valid():
+                            image = new_image.save(commit=False)
+                            if traitement.type_traitement == "Image":
+                                image.is_qrcode = False
+                            elif traitement.type_traitement == "QR-Code":
+                                image.is_qrcode = True
+                                
+                            image.save()
+                            traitement.image = image
+                        else:
+                            messages.error(request, 'Erreur : L\'image que vous avez entrer ne peut pas être acceptée !')
+                    print('<Texte ??>',traitement.type_traitement, file=sys.stderr)
+                    traitement.modele3D = new_modele
+                    traitement.save()
+                    messages.success(request, ('Le modele AR a été ajouté !'))
+                # else:
+                #     return redirect('add_traitement')
+            else:
+                messages.error(request, 'Erreur : Le modèle ne peut pas être enregistré !')
+        return redirect("chapitres_list")
+    
+    return render(request, 'cours/add_traitement.html', context={'new_traitement': new_traitement, 'new_modele3d': new_modele3d, 'new_image': new_image, 'new_file': new_file
+                                                                #  , 'trait': trait
+                                                                 })
+
+
+def file_upload_location(modele3d, filename):
+        
+        path_modele3d = modele3d.path_modele3d
+        return '%s/%s' % (path_modele3d, filename)
+    
+def model_location(modelName):
+        now = time.time()
+        stamp = datetime.datetime.fromtimestamp(now).strftime('%Y-%m-%d-%H-%M-%S')
+        return 'img/cours/modeles_3d/%s-%s' % (modelName, str(stamp))  
+
+def handle_file_upload(f, new_modele):
+    with open(file_upload_location(new_modele,f.name), 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+def makedirs(path):
+    # os.mkdir(os.path.join('/media/', path))
+    try:
+        os.makedirs(os.path.join('/media/', path))
+    except OSError as e:
+        if e.errno == 17:
+            pass
 
