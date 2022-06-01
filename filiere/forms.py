628a130d9764ba07214f509b6189ae980698b9f1
kaoutar-ou <kaoutar.oubenaddi@gmail.com>
2022-06-01 17:05:07 +0100

    UnivIt : Fixing Errors

--- filiere/forms.py
+++ filiere/forms.py
@@ -2,16 +2,16 @@ from django import forms
 
 
 class FiliereForm(forms.Form):
-    nom_filiere = forms.CharField(max_length=30)
-    etablissement = forms.CharField(max_length=5)
+    nom_filiere = forms.CharField(max_length=50)
+    etablissement = forms.CharField(max_length=100)
     logo = forms.ImageField(required=False)
 
+
 class EtablissementForm(forms.Form):
-    nom = forms.CharField(max_length=30)
-    adresse = forms.CharField(max_length=30)
+    nom = forms.CharField(max_length=50)
+    adresse = forms.CharField(max_length=300)
     telephone = forms.CharField(max_length=13)
     logo = forms.ImageField(required=False)
-    niveau=forms.CharField(max_length=100)
-    website=forms.URLField(max_length=200)
-    email=forms.EmailField(max_length=254)
-    
+    niveau = forms.CharField(max_length=100)
+    website = forms.URLField(max_length=200)
+    email = forms.EmailField(max_length=254)
